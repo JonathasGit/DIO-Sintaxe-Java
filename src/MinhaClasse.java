bbb3c40aab4a3d1d04483c92aa12bb8ed6af5e76
JonathasGit <Jonathas-25@hotmail.com>
2023-01-08 01:11:12 -0300

    #2 Segunda aula

--- src/MinhaClasse.java
+++ src/MinhaClasse.java
@@ -3,6 +3,7 @@ public class MinhaClasse {
     public static void main(String[] args) {
         
         System.out.println("Hello World");
+        System.out.println("Teste Commit 2");
     }
     
 }
