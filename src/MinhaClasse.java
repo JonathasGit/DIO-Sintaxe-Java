c9c3dc67f5392b79c61d7c4f23d24871d6f585b5
JonathasGit <Jonathas-25@hotmail.com>
2023-01-08 09:06:41 -0300

    #2 Aula 2 de anatomia

--- src/MinhaClasse.java
+++ src/MinhaClasse.java
@@ -4,6 +4,12 @@ public class MinhaClasse {
         
         System.out.println("Hello World");
         System.out.println("Teste Commit 2");
+
+        int ano = 2022;
+        final String BR = "Brasil"; //final para nunca ser alterado
+
+        ano = 2023;
     }
     
 }
+//Aula 2 de anatomia, falando de Variavel 
