92a8bc70c4b146591c177be4f754f7ba68fab935
JonathasGit <Jonathas-25@hotmail.com>
2023-01-09 19:41:48 -0300

    #2 Aula 3 criando metodo

--- src/MinhaClasse.java
+++ src/MinhaClasse.java
@@ -4,12 +4,42 @@ public class MinhaClasse {
         
         System.out.println("Hello World");
         System.out.println("Teste Commit 2");
+        ;
 
         int ano = 2022;
         final String BR = "Brasil"; //final para nunca ser alterado
 
         ano = 2023;
+
+
+        
+        // Aula 3 - Tipos de Variaveis
+        String meuNome = "Jonathas";
+        int anoDeFabricacao = 2023;
+        boolean verdadeira = true;
+        System.out.println(anoDeFabricacao);
+
+
+
+        //alterar variavel
+
+        anoDeFabricacao = 2018;
+        System.out.println(anoDeFabricacao);
+
+        String primeiroNome = "Teste";
+        String segundoNome = "Teste2";
+
+        String nomeCompleto2 = nomeCompleto (primeiroNome, segundoNome); // chamando o metodo
+        System.out.println(nomeCompleto2);
+
     }
+
+    //Criando um metodo
+
+    public static String nomeCompleto (String primeiroNome, String segundoNome){
+        return primeiroNome.concat(" ").concat(segundoNome);
+    }
+
     
 }
 //Aula 2 de anatomia, falando de Variavel 
