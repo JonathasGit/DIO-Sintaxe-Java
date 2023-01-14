352a9ab4db532a4fa970a01df99a8f784277963b
JonathasGit <Jonathas-25@hotmail.com>
2023-01-13 22:55:58 -0300

    #2 tipos e variaveis 3

---
+++ variavies-java/src/variaveis3.java
@@ -0,0 +1,18 @@
+public class variaveis3 {
+    public static void main(String[] args) {
+
+        byte idade = 123;
+        short ano = 2023; 
+        int cep = 00000000;
+        long cpf = 00000000000L;
+        float pi = 3.14;
+        double salario = 1275.33;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = numeroNormal;
+        
+    }
+
+    
+}
