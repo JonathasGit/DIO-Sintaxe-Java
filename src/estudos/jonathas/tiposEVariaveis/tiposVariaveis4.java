c22301ba090cc138e27d690658f8faef835dd4ea
JonathasGit <Jonathas-25@hotmail.com>
2023-01-14 23:11:42 -0300

    #2 Aula 5

---
+++ src/estudos/jonathas/tiposEVariaveis/tiposVariaveis4.java
@@ -0,0 +1,18 @@
+package estudos.jonathas.tiposEVariaveis;
+
+public class tiposVariaveis4 {
+    public static void main(String[] args) {
+
+        int numero = 1; 
+
+        numero = 2; 
+
+        
+        System.out.println("Teste");
+        System.out.println(numero);
+
+        final double VALOR_DE_PI = 3.14; //Para fixar o valor
+
+    }
+    
+}
---
+++ variavies-java/src/VariaveiseTipos4.java
@@ -0,0 +1,7 @@
+public class VariaveiseTipos4 {
+
+    public static void main(String[] args) {
+        
+    }
+    
+}
