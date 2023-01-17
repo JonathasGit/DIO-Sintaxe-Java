9102bef8d2b56b886af353fb9f2b9af2fad9a2f8
JonathasGit <Jonathas-25@hotmail.com>
2023-01-16 23:22:22 -0300

    #2 Operdaores aula 2

---
+++ src/estudos/jonathas/tiposEVariaveis/operadores2.java
@@ -0,0 +1,25 @@
+public class operadores2 {
+
+    public static void main(String[] args) {
+        String nome ="AUla DIO";
+        int idade = 27;
+        double peso = 68.5;
+        char sexo = 'M';
+        boolean doadorOrgao = false;
+        //Date dataNascimento1 = new Date(); // classe que representa tipo
+
+
+        ///OPerador Aritmeticos
+
+        double soma = 10.5 + 15.7;
+        int subtração = 113 - 25;
+        int multiplicacao = 20 * 7; 
+        int divisao = 15 / 3;
+        int modulo = 18 % 8; //Resto de divisão 
+        double resultado = (10*7) + (20/4);
+
+
+
+    }
+    
+}
