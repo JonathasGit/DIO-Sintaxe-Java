078fa50956f6ee783f9727e33b0e4845203a0a05
Asus Jothur <Jonathas-25@hotmail.com>
2023-01-17 23:20:08 -0300

    Operadores 4

---
+++ src/estudos/jonathas/tiposEVariaveis/operadores4.java
@@ -0,0 +1,24 @@
+package estudos.jonathas.tiposEVariaveis;
+
+public class operadores4 {
+    public static void main(String[] args) {
+        int numero = 5; 
+        System.out.println(numero);
+
+        //numero = numero +2;
+        numero ++;
+
+        System.out.println( numero ++);
+        System.out.println( numero --);
+        System.out.println( numero);
+
+        //negar valor boolean
+
+        boolean variavel = true;
+
+        System.out.println(!variavel);
+
+
+    }
+    
+}
